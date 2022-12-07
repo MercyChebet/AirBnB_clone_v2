2aabfc5dd7c67dfbbcbd8cc05b31cae712b4f859
yrncollo <ngenocollins599@gmail.com>
2022-12-07 15:21:26 +0300

    Add or replace in the class BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,18 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from os import getenv
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        id = Column(String(60), primary_key=True, nullable=False)
+        created_at = Column(DateTime, default=datetime.utcnow(),
+                            nullable=False)
+        updated_at = Column(DateTime, default=datetime.utcnow(),
+                            nullable=False)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
