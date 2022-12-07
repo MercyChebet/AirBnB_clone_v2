251912b0eda19163e6d9730f7307f7d7eb5024bc
yrncollo <ngenocollins599@gmail.com>
2022-12-07 14:42:54 +0300

    Update FileStorage: (models/engine/file_storage.py)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,40 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """
+        Deletes the specified object from the list of objects if it exists.
+        If obj is None, the method does nothing.
+        """
+        if obj is not None:
+            # check if the object is in the list of objects
+            if obj in self.__objects.values():
+                # delete the object from the list of objects
+                del self.__objects[obj.id]
+                # save the changes to the JSON file
+                self.save()
+
+    def all(self, cls=None):
+        """
+        Returns a list of all objects in the storage.
+        If cls is specified, the list will only contain objects of the given 
+        class.
+        """
+        # if no class is specified, return a list of all objects
+        if cls is None:
+            return list(self.__objects.values())
+
+        # if a class is specified, return a list of objects of that class
+        else:
+            # create an empty list for objects of the given class
+            obj_list = []
+            # iterate over the objects in the storage
+            for obj in self.__objects.values():
+                # check if the object is an instance of the given class
+                if isinstance(obj, cls):
+                    # add the object to the list
+                    obj_list.append(obj)
+
+            return obj_list
+
