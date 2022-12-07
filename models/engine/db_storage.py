cb8195fd42bf43a973d00e7d41d77ebca08203b8
yrncollo <ngenocollins599@gmail.com>
2022-12-07 22:19:53 +0300

    This class manages storage of hbnb database

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -14,3 +14,25 @@ from models.base_model import Base
 classes = {"Amenity": Amenity, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
+
+class DBStorage:
+    """ This class manages storage of hbnb database """
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """ instantiates DBStorage object """
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        HBNB_ENV = getenv('HBNB_ENV')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(HBNB_MYSQL_USER,
+                                              HBNB_MYSQL_PWD,
+                                              HBNB_MYSQL_HOST,
+                                              HBNB_MYSQL_DB),
+                                      pool_pre_ping=True)
+
+        if HBNB_ENV == "test":
+            Base.metadata.drop_all(self.__engine)
