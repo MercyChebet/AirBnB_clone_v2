76d685689fb1c725d9581a23aaefe40cc744a3f4
yrncollo <ngenocollins599@gmail.com>
2022-12-07 15:41:17 +0300

    class attribute id

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,8 @@ import uuid
 from datetime import datetime
 from os import getenv
 import models
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
 if getenv('HBNB_TYPE_STORAGE') == "db":
     Base = declarative_base()
@@ -11,6 +13,7 @@ else:
     class Base:
         """useless class"""
         pass
+
 
 class BaseModel:
     """A base class for all hbnb models"""
@@ -22,13 +25,11 @@ class BaseModel:
                             nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
