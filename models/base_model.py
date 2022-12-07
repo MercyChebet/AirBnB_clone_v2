095ff58601b2b0a6f7f5c63116f8b9665eb00d25
yrncollo <ngenocollins599@gmail.com>
2022-12-07 15:42:58 +0300

    class attribute created_at

--- models/base_model.py
+++ models/base_model.py
@@ -31,12 +31,14 @@ class BaseModel:
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            if 'id' not in kwargs:
+                kwargs['id'] = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                kwargs['created_at'] = datetime.now()
+            elif not isinstance(kwargs['created_at'], datetime):
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                         "%Y-%m-%d %H:%M:%S.%f"
+                                                         )
 
     def __str__(self):
         """Returns a string representation of the instance"""
