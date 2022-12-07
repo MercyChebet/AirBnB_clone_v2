1504dddf16074f1903bf870cc3452328e3a0ef42
yrncollo <ngenocollins599@gmail.com>
2022-12-07 22:07:11 +0300

    added imports

--- models/state.py
+++ models/state.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
+from models.base_model import BaseModel, Base
+import models
+from models.city import City
 
 
 class State(BaseModel):
