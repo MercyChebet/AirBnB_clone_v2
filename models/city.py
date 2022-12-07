9944fb2ddd0e0f2ca8ce0dfca3b2c22f7e792603
yrncollo <ngenocollins599@gmail.com>
2022-12-07 21:59:47 +0300

    added base model import

--- models/city.py
+++ models/city.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+from models.base_model import BaseModel, Base
+
+
 
 class City(BaseModel, Base):
     """City class"""
