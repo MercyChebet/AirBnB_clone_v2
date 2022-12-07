155a1f749803447357f53e6be48447951edaa3c5
yrncollo <ngenocollins599@gmail.com>
2022-12-07 14:09:15 +0300

    create the hbnb_test user if it doesn't exist

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,2 +1,3 @@
 -- Script that prepares a MySQL server for the project tests
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
