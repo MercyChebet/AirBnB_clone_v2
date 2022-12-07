5514b771c3f01c0299621d756945a3e46641a2fc
yrncollo <ngenocollins599@gmail.com>
2022-12-07 22:10:34 +0300

    class attribute state_id

--- models/state.py
+++ models/state.py
@@ -16,3 +16,13 @@ class State(BaseModel, Base):
         cities = relationship("City", backref="state")
     else:
         name = ""
+
+        @property
+        def cities(self):
+            """ getter for list of cities in the state """
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
