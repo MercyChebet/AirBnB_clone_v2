5aaaac77216c1c60b56f9e26babe3f9b23735a75
MercyChebet <chebetm96@gmail.com>
2023-01-15 21:41:16 +0300

    One

--- models/state.py
+++ models/state.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from os import getenv
-from models.base_model import BaseModel, Base
+"""State module for HBNB project"""
+
 import models
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from models.city import City
 
 
 class State(BaseModel, Base):
-    """ State class """
-    if getenv('HBNB_TYPE_STORAGE') == "db":
-        __tablename__ = "states"
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-    else:
-        name = ""
+    """State class for HBNB project"""
 
+    __tablename__ = 'states'
+
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship(
+            'City',
+            cascade='all, delete, delete-orphan',
+            backref='state'
+        )
+    else:
         @property
         def cities(self):
-            """ getter for list of cities in the state """
-            city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
-
-    def __init__(self, *args, **kwargs):
-        """ initializes State """
-        super().__init__(*args, **kwargs)
+            """Returns the cities in this State"""
+            cities_in_state = []
+            for value in models.storage.all(City).values():
+                if value.state_id == self.id:
+                    cities_in_state.append(value)
+            return cities_in_state
